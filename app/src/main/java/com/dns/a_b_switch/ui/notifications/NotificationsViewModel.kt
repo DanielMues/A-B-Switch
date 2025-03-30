3fe81073c56d9a4d81c6a5a2a38ad2924be7ff99
Sasha <s.marchuk95@gmail.com>
2025-03-30 13:33:59 +0200

    Initial Setup

---
+++ app/src/main/java/com/dns/a_b_switch/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.dns.a_b_switch.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
